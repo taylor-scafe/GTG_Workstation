6784c473ea0a3ecf7a35455064f16e1c2a4194bb
jolhill <joshua.hill55@gmail.com>
2016-03-29 15:44:50 -0500

    Convert Calc Panel
    Moved Math class to an inner class of the calculator

--- src/GTG_MainGUI.java
+++ src/GTG_MainGUI.java
@@ -2,7 +2,6 @@ import javax.swing.JFrame;
 
 public class GTG_MainGUI {
 	public static void main (String[] args) {
-		GTG_NumPad numPad = new GTG_NumPad();
-		numPad.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		int filler = 0;
 	}	
 }
--- src/GTG_NumPad.java
+++ src/GTG_NumPad.java
@@ -35,30 +35,28 @@ import java.awt.Font;
  *  -MAKE CALCULATION LIVE
  */
 @SuppressWarnings("serial")
-public class GTG_NumPad extends JFrame {
+public class GTG_NumPad extends JPanel {
 	
 	private JButton btnDigit1, btnDigit2, btnDigit3, btnDigit4, btnDigit5, btnDigit6, btnDigit7, btnDigit8, btnDigit9, btnDigit0;
 	private JButton btnADD, btnSUBTRACT, btnBackspace, btnMULTPLY, btnClear, btnLeftParen, btnRightParen, btnExecute;
 	JTextField currentExpr,incomingData, result;
 	JLabel lblOperator, lblEquals;
-	private JPanel NumPad;
+	
 	private ButtonHandler bh;
 	private JRadioButton rdbtnSet,rdbtnAdd,rdbtnSub;
 	private ButtonGroup operatorGroup;
 	public GTG_NumPad() {
-		super("GTG Calculator");
-		NumPad = new JPanel();
-		NumPad.setBackground(Color.BLUE);
-		NumPad.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(NumPad);
-		NumPad.setLayout(new BorderLayout(0, 0));
+		//super("GTG Calculator");
+		setBackground(Color.BLUE);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(new BorderLayout(0, 0));
 
 
 		//--------------Number Panel-----------------------------------------------
 		this.bh = new ButtonHandler();
 		this.addKeyListener(bh);
 		JPanel NumPadPanel = new JPanel();
-		NumPad.add(NumPadPanel, BorderLayout.CENTER);
+		add(NumPadPanel, BorderLayout.CENTER);
 		NumPadPanel.setLayout(new GridLayout(0, 3, 0, 0));
 		
 		btnDigit1 = new JButton("1");
@@ -139,7 +137,7 @@ public class GTG_NumPad extends JFrame {
 		//--------------Operations Panel-------------------------------------------
 
 		JPanel OperationPanel = new JPanel();
-		NumPad.add(OperationPanel, BorderLayout.SOUTH);
+		add(OperationPanel, BorderLayout.SOUTH);
 
 		operatorGroup = new ButtonGroup();
 		OperationPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
@@ -168,7 +166,7 @@ public class GTG_NumPad extends JFrame {
 
 		JPanel DatafieldPanel = new JPanel();
 		DatafieldPanel.setBackground(new Color(51,204,255));
-		NumPad.add(DatafieldPanel, BorderLayout.NORTH);
+		add(DatafieldPanel, BorderLayout.NORTH);
 
 		incomingData = new JTextField(6);
 		incomingData.setBackground(Color.lightGray);
@@ -199,7 +197,7 @@ public class GTG_NumPad extends JFrame {
 		result.setText("0");
 		DatafieldPanel.add(result);
 
-		pack();
+		//pack();
 		setVisible(true);
 
 		//------------Action Handlers----------------------------------------------
@@ -279,5 +277,239 @@ public class GTG_NumPad extends JFrame {
 			}
 		}catch(Exception e){e.printStackTrace();}
 		return false;
+	}
+	
+
+	private static class math{
+		private static final String numbers = "1234567890";
+		private static final String symbols = "+-*/()";
+		
+		public static int eval(ArrayList<Character> aExpr) throws NumberFormatException{
+			int iResult = 0;
+			boolean notDone = true;
+			while(notDone){
+				//___________________PARENTHESES_____________________
+				if(aExpr.indexOf('(') != -1){
+					try{
+						if(!symbols.contains("" + aExpr.get(aExpr.indexOf('(') -1))){
+							aExpr.add(aExpr.indexOf('('), '*');
+						}
+					}catch(Exception e){}
+					int iBegin = aExpr.indexOf('(');
+					int iEnd = aExpr.indexOf(')');
+					if(iEnd == -1){
+						throw new NumberFormatException();
+					}
+					int iLen = iEnd - iBegin;
+					ArrayList<Character> newList = new ArrayList<Character>();
+					//Removes '('
+					aExpr.remove(iBegin);
+					for(int i = 0; i < iLen - 1; i++){
+						newList.add(aExpr.remove(iBegin));
+					}
+					//Removes ')'
+					aExpr.remove(iBegin);
+					//INSERT SOMEWHERE INTO CURRENT INSTANCE
+					//Recursive call
+					int iSubResult = math.eval(newList);
+					String sResult = "" + (iSubResult);
+					//System.out.println(sResult);
+					for(int i = sResult.length()-1; i >= 0; i-- ){
+						aExpr.add(iBegin, sResult.charAt(i));
+					}
+				}
+				//___________________MULTIPLICATION_____________________
+				
+				else if(aExpr.indexOf('*') != -1){	
+		
+					try{
+						int iopLocation = (aExpr.indexOf('*'));
+						int iStart = iopLocation -1;
+						
+						//GETS FIRST OPPERANT
+						while(true){
+							try{
+								Integer.parseInt("" + aExpr.get(iStart -1));
+								iStart--;
+							}catch(Exception e){break;}
+						}
+						//int iOperand1 = Integer.parseInt(""+(aExpr.remove(iStart)));
+						String sOperand1 = "";
+						for(int i = 0; i < iopLocation-iStart; i++){
+							sOperand1 += aExpr.remove(iStart);
+						}
+						//System.out.println("op 1 :" + sOperand1);
+						int iOperand1 = Integer.parseInt(sOperand1);
+						
+						//System.out.println("op1: " + iOperand1);
+						
+						//REMOVES OPERATOR
+						aExpr.remove(iStart);
+						
+						int iEnd = iStart;
+						//GETS SECOND OPPERANT
+						while(true){
+							try{
+								Integer.parseInt("" + aExpr.get(iEnd +1));
+								iEnd++;
+							}catch(Exception e){break;}
+						}
+						
+						String sOperand2 = "";
+						for(int i = 0; i <= iEnd-iStart; i++){
+							sOperand2 += aExpr.remove(iStart);
+						}
+						//System.out.println("op 2 :" + sOperand2);
+						int iOperand2 = Integer.parseInt(sOperand2);
+						
+						//int iOperand2 = Integer.parseInt(""+(aExpr.remove(iStart)));
+						//System.out.println("op2: " + iOperand2);
+						
+						
+						
+						String sResult = "" + (iOperand1 * iOperand2);
+						//System.out.println(sResult);
+						for(int i = sResult.length() - 1; i >= 0; i-- ){
+							//System.out.println("I:" + i + " Char @: "  + sResult.charAt(i));
+							aExpr.add(iStart, sResult.charAt(i));
+						}
+					}catch(NumberFormatException e){
+						JOptionPane.showMessageDialog(null, "Invalid Input");
+					}
+	
+				}
+				
+				
+				//___________________ADDITION AND SUBTRACTION_____________________
+			
+				else if(aExpr.indexOf('+') != -1 || aExpr.indexOf('-') != -1){
+					//Addition
+					//If + symbol comes before - or if there is a + and no -
+					if((aExpr.indexOf('+') != -1 && aExpr.indexOf('+') < aExpr.indexOf('-'))
+					   ||(aExpr.indexOf('+') != -1 && aExpr.indexOf('-') == -1)){
+						
+						try{
+							int iopLocation = (aExpr.indexOf('+'));
+							int iStart = iopLocation -1;
+							
+							//GETS FIRST OPPERANT
+							while(true){
+								try{
+									Integer.parseInt("" + aExpr.get(iStart -1));
+									iStart--;
+								}catch(Exception e){break;}
+							}
+							//int iOperand1 = Integer.parseInt(""+(aExpr.remove(iStart)));
+							String sOperand1 = "";
+							for(int i = 0; i < iopLocation-iStart; i++){
+								sOperand1 += aExpr.remove(iStart);
+							}
+							//System.out.println("op 1 :" + sOperand1);
+							int iOperand1 = Integer.parseInt(sOperand1);
+							
+							//System.out.println("op1: " + iOperand1);
+							
+							//REMOVES OPERATOR
+							aExpr.remove(iStart);
+							
+							int iEnd = iStart;
+							//GETS SECOND OPPERANT
+							while(true){
+								try{
+									Integer.parseInt("" + aExpr.get(iEnd +1));
+									iEnd++;
+								}catch(Exception e){break;}
+							}
+							
+							String sOperand2 = "";
+							for(int i = 0; i <= iEnd-iStart; i++){
+								sOperand2 += aExpr.remove(iStart);
+							}
+							//System.out.println("op 2 :" + sOperand2);
+							int iOperand2 = Integer.parseInt(sOperand2);							String sResult = "" + (iOperand1 + iOperand2);
+							//System.out.println(sResult);
+							for(int i = sResult.length() - 1; i >= 0; i-- ){
+								aExpr.add(iStart, sResult.charAt(i));
+							}
+							//System.out.println("A-L len: " + aExpr.size());
+						}catch(NumberFormatException e){
+							JOptionPane.showMessageDialog(null, "Invalid Input");
+						}
+						
+					}
+					//SUB
+					else{
+						try{
+							int iopLocation = (aExpr.indexOf('-'));
+							int iStart = iopLocation -1;
+							
+							//GETS FIRST OPPERANT
+							while(true){
+								try{
+									Integer.parseInt("" + aExpr.get(iStart -1));
+									iStart--;
+								}catch(Exception e){break;}
+							}
+							//int iOperand1 = Integer.parseInt(""+(aExpr.remove(iStart)));
+							String sOperand1 = "";
+							for(int i = 0; i < iopLocation-iStart; i++){
+								sOperand1 += aExpr.remove(iStart);
+							}
+							//System.out.println("op 1 :" + sOperand1);
+							int iOperand1 = Integer.parseInt(sOperand1);
+							
+							//System.out.println("op1: " + iOperand1);
+							
+							//REMOVES OPERATOR
+							aExpr.remove(iStart);
+							
+							int iEnd = iStart;
+							//GETS SECOND OPPERANT
+							while(true){
+								try{
+									Integer.parseInt("" + aExpr.get(iEnd +1));
+									iEnd++;
+								}catch(Exception e){break;}
+							}
+							
+							String sOperand2 = "";
+							for(int i = 0; i <= iEnd-iStart; i++){
+								sOperand2 += aExpr.remove(iStart);
+							}
+							//System.out.println("op 2 :" + sOperand2);
+							int iOperand2 = Integer.parseInt(sOperand2);
+							String sResult = "" + (iOperand1 - iOperand2);
+							//System.out.println(sResult);
+							for(int i = sResult.length() - 1; i >= 0; i-- ){
+								aExpr.add(iStart, sResult.charAt(i));
+							}
+							//System.out.println("A-L len: " + aExpr.size());
+						}catch(NumberFormatException e){
+							JOptionPane.showMessageDialog(null, "Invalid Input");
+						}
+					}
+				}
+				else{
+					//System.out.println("test3");
+					for(char c : aExpr){
+						if(math.numbers.indexOf( "" + c) == -1 ){
+							//System.out.println("test1");
+							throw new NumberFormatException();
+						}
+						else{
+							//System.out.println("test2 :" + c);
+							String sResult = "";
+							for(int i = 0; i < aExpr.size(); i++){
+								sResult += aExpr.get(i);
+							}
+							iResult = Integer.parseInt(sResult);
+						}
+					}
+					notDone = false;
+				}
+						
+			}
+			return iResult;
+		}
 	}
 }
